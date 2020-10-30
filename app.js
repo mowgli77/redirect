e337a2e4393f66bb001fdba74e725ec94c50e42b
Petro Khanenko <p.khanenko@gmail.com>
2020-10-30 02:29:05 +0200

    fucking commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+if (process.env.NODE_ENV === 'production') {
+    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
+const PORT = process.env.PORT || 5000
+
+function start() {
+    app.listen(PORT, () => {
+        console.log(`Server is started on port ${PORT}`)
+    })
+}
+
+start()
---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './App.css'
+import {Route, BrowserRouter} from "react-router-dom";
+
+function App() {
+
+    return (
+        <BrowserRouter>
+            <div className={"loader"}>
+                <Route path='/' component={() => { window.location = 'https://mnogomebeli.com/about/salony/map/'; return null;} }/>
+                <div className="lds-ring">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+        </BrowserRouter>
+    )
+}
+
+export default App
