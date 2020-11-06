38071a052a04a1068ec870b3b6be0da5102b3f6f
Petro Khanenko <p.khanenko@gmail.com>
2020-11-06 20:07:04 +0200

    fuck off commit!!!

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     return (
         <BrowserRouter>
             <div className={"loader"}>
-                <Route path='/' component={() => { window.location = 'https://mnogomebeli.com/'; return null;} }/>
+                <Route path='/' component={() => { window.location = 'https://ad.admitad.com/g/ek1lob0vrw561b9069146818ed5561/'; return null;} }/>
                 <div className="lds-ring">
                     <div></div>
                     <div></div>
